fd928f90c5e844b05250794379eba0b0944b0b81
Luis Romero <luromeromx31@gmail.com>
2020-05-11 00:20:57 -0500

    Sintaxis Básica II

--- HolaMundo.py
+++ HolaMundo.py
@@ -6,3 +6,4 @@ a=0
 for i in range(5):
     a+=1
     print(a)
+#    print(5+6)
---
+++ operadores.py
@@ -0,0 +1,31 @@
+print("Operador Suma:")
+print(5+5)
+
+print("Operador %, regresa el modulo de una division:")
+print(10%3)
+
+print("Exponente: ")
+print(5**3)
+
+print("divicion, regresa el vamor entero")
+print(12//4)
+
+nombre=5
+print(type(nombre))
+nombre="Luis"
+print(type(nombre))
+nombre=2.5
+print(type(nombre))
+
+mensaje="""Esto es un mensaje
+con tres saltos
+de linea"""
+print(mensaje)
+
+print("Prueba del primer IF")
+numero1=3
+numero2=2
+if numero1<numero2:
+    print("El numero1 es menor que numero2")
+else:
+    print("El numero2 es menor que el numero1")
