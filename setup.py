be1d6a6cbdb5de702538e1602b463818bb39be6e
Luis Romero <luromeromx31@gmail.com>
2020-05-24 10:12:03 -0500

    Distribuidos

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+
+    name="Paquetecalculos",
+    version="1.0",
+    description="Paquete de redondeo",
+    author="Luis",
+    author_email="luromero@hotmail.com",
+    url="www.google.com",
+    packages=["Modulos","Modulos.Redondeo"]
+)
